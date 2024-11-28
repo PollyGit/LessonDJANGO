6d682945409ab6b4bfe59d9992959d37440f35f8
PollyGit <63475486+PollyGit@users.noreply.github.com>
2024-11-28 18:28:48 +0300

    copy

---
+++ LessonDJ02/main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+# импортируем views.py из этой же папки
+from . import views
+
+# подключаем файл views.py для отображения страниц HTML
+# index - ф-ция из views.py которая будет выполняться при
+# переходе на главную страницу проекта
+urlpatterns = [
+    path('', views.index),
+    path('new', views.new),
+    path('data', views.data),
+    path('test', views.test)
+]
